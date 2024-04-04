7bc4d6b3d19a597d7f9cb0fb1acb7bcd4788ef10
Damian Trafialek <damian.trafialek@gmail.com>
2024-04-04 15:35:43 +0200

    finished model but is incorrect xdd

--- advanced_ml_model.py
+++ advanced_ml_model.py
@@ -14,6 +14,24 @@ from utils.logger import logger
 
 logger.name = __name__
 
+submission = pd.read_csv("advanced_ml_model_submission.csv")
+submission["sales"] = submission["sales"].clip(0.0)
+submission.to_csv("advanced_ml_model_submission_2.csv", index=False)
+
+# --- CONFIG ---
+DISABLE_CHECKING = True
+CONSIDER_HOLIDAYS = False
+CONSIDER_ONPROMOTION = False
+EARTH_QUAKE_IMPACT_PERIOD = 90
+OPTIMIZE_WITH_OPTUNA = False
+
+TRAIN_START = "2015-01-01"
+TRAIN_END = "2017-06-30"
+TEST_START = "2017-07-01"
+TEST_END = "2017-07-31"
+QUERY_START = "2017-08-01"
+QUERY_END = "2017-08-15"
+
 # --- LOAD DATA ---
 logger.info("Loading data...")
 holidays_events, oil, stores, transactions, store_sales, query = load_files()
@@ -66,14 +84,35 @@ combined_sales["days_since_last_paycheck"] = combined_sales[
     "days_since_last_paycheck"
 ].astype("uint8")
 
-combined_sales["is_holiday"] = combined_sales.index.get_level_values("date").isin(
-    holidays_events[
-        holidays_events["transferred"].eq(False)
-        & holidays_events["locale"].isin(["National"])
-    ].index
-)
 
-combined_sales["is_holiday"] = combined_sales["is_holiday"].astype("category")
+if CONSIDER_HOLIDAYS:
+    combined_sales["is_holiday"] = combined_sales.index.get_level_values("date").isin(
+        holidays_events[
+            holidays_events["transferred"].eq(False)
+            & holidays_events["locale"].isin(["National"])
+        ].index
+    )
+
+    combined_sales["is_holiday"] = combined_sales["is_holiday"].astype("category")
+    lag_holiday = make_lags(
+        combined_sales["is_holiday"], lags=2, name="is_holiday"
+    ).bfill()
+    lead_holiday = make_leads(
+        combined_sales["is_holiday"], leads=16, name="is_holiday"
+    ).ffill()
+
+
+if CONSIDER_ONPROMOTION:
+    lag_promotion = make_lags(
+        combined_sales["onpromotion"], lags=2, name="onpromotion"
+    ).bfill()
+    lead_promotion = make_leads(
+        combined_sales["onpromotion"], leads=16, name="onpromotion"
+    ).ffill()
+else:
+    combined_sales = combined_sales.drop(columns=["onpromotion"])
+
+
 combined_sales["month"] = combined_sales["month"].astype("category")
 combined_sales["day_of_week"] = combined_sales["day_of_week"].astype("category")
 combined_sales["year"] = combined_sales["year"].astype("category")
@@ -88,54 +127,68 @@ combined_sales["dcoilwtico"] = (
 # People rallied in relief efforts donating water and other first need products which greatly affected supermarket sales for several weeks after the earthquake.
 # --- NOTICE ---
 # pd.perdiod_range must be used to create a range of dates because the dates are treated as period
-EARTH_QUAKE_IMPACT_PERIOD = 90
+
 combined_sales["eartquake_impact"] = combined_sales.index.get_level_values("date").isin(
     pd.period_range("2016-04-16", periods=EARTH_QUAKE_IMPACT_PERIOD)
 )
 lead_oil = make_leads(combined_sales["dcoilwtico"], leads=16, name="dcoilwtico").ffill()
-lead_holiday = make_leads(
-    combined_sales["is_holiday"], leads=16, name="is_holiday"
-).ffill()
-lead_promotion = make_leads(
-    combined_sales["onpromotion"], leads=16, name="onpromotion"
-).ffill()
-lag_target = make_lags(combined_sales["sales"], lags=4, name="sales").bfill()
-lag_promotion = make_lags(
-    combined_sales["onpromotion"], lags=2, name="onpromotion"
-).bfill()
-lag_holiday = make_lags(combined_sales["is_holiday"], lags=2, name="is_holiday").bfill()
 
-combined_sales_lead_lag = (
-    combined_sales.join(lead_oil)
-    .join(lead_holiday)
-    .join(lead_promotion)
-    .join(lag_target)
-    .join(lag_promotion)
-    .join(lag_holiday)
-)
+
+lag_target = make_lags(combined_sales["sales"], lags=4, name="sales").bfill()
+
+
+combined_sales_lead_lag = combined_sales.join(lead_oil).join(lag_target)
+
+if CONSIDER_HOLIDAYS:
+    combined_sales_lead_lag = combined_sales_lead_lag.join(lead_holiday).join(
+        lag_holiday
+    )
+
+if CONSIDER_ONPROMOTION:
+    combined_sales_lead_lag = combined_sales_lead_lag.join(lead_promotion).join(
+        lag_promotion
+    )
 
 
 # --- STANDARDIZE AND ENCODE DATA ---
 logger.info("Standardizing and encoding data...")
+
 col_to_scale = [
     "dcoilwtico",
-    "sales",
-    "onpromotion",
     "days_since_last_paycheck",
     *lead_oil.columns,
     *lag_target.columns,
-    *lag_promotion.columns,
-    *lead_promotion.columns,
 ]
+
+col_to_passthrough = [
+    "id",
+    "sales",
+]
+
+
 col_to_encode = [
     "month",
     "day_of_week",
     "year",
-    "is_holiday",
     "eartquake_impact",
-    *lead_holiday.columns,
-    *lag_holiday.columns,
 ]
+
+if CONSIDER_ONPROMOTION:
+    col_to_encode = [
+        *col_to_encode,
+        "onpromotion",
+        *lead_promotion.columns,
+        *lag_promotion.columns,
+    ]
+
+if CONSIDER_HOLIDAYS:
+    col_to_encode = [
+        *col_to_encode,
+        "is_holiday",
+        *lead_holiday.columns,
+        *lag_holiday.columns,
+    ]
+
 
 scaler = StandardScaler()
 scaler.fit(combined_sales_lead_lag[col_to_scale])
@@ -147,14 +200,13 @@ col_scaled = pd.DataFrame(
 col_encoded = pd.get_dummies(combined_sales_lead_lag[col_to_encode], drop_first=True)  # type: ignore
 
 combined_sales_final = pd.concat(
-    [col_scaled, col_encoded, combined_sales_lead_lag["id"]], axis=1
+    [col_scaled, col_encoded, combined_sales_lead_lag[col_to_passthrough]], axis=1
 )
 
 
 # --- CHECK FEATURES ---
 logger.info("Checking features...")
 
-DISABLE_CHECKING = True
 omit_col = [*combined_sales.select_dtypes("category").columns, "id"]
 num_col = list(set(list(combined_sales.columns)) - set(omit_col))
 
@@ -183,10 +235,6 @@ else:
 # --- SPLIT DATA ---
 logger.info("Splitting data...")
 
-TRAIN_START = "2013-01-01"
-TRAIN_END = "2016-12-31"
-TEST_START = "2017-01-01"
-TEST_END = "2017-07-31"
 
 X = combined_sales_final.drop(columns=["sales", "id"])
 y = make_multistep_target(combined_sales_final["sales"], steps=16)
@@ -194,39 +242,103 @@ y = make_multistep_target(combined_sales_final["sales"], steps=16)
 X_train, y_train = X.loc[TRAIN_START:TRAIN_END], y.loc[TRAIN_START:TRAIN_END]
 X_test, y_test = X.loc[TEST_START:TEST_END], y.loc[TEST_START:TEST_END]
 
+X_final, y_final = X.loc[TRAIN_START:TEST_END], y.loc[TRAIN_START:TEST_END]
+X_query = X.loc[QUERY_START:QUERY_END]
+
 # --- TRAIN MODEL WITH OPTUNA ---
-logger.error("Training model...")
+logger.info("Training model...")
 
+if OPTIMIZE_WITH_OPTUNA:
 
-def objective(trial):
-    params = {
-        "n_estimators": trial.suggest_int("n_estimators", 100, 1000, step=100),
-        "max_depth": trial.suggest_int("max_depth", 3, 10),
-        "learning_rate": trial.suggest_loguniform("learning_rate", 0.001, 0.1),
-        "subsample": trial.suggest_uniform("subsample", 0.6, 1.0),
-        "colsample_bytree": trial.suggest_uniform("colsample_bytree", 0.6, 1.0),
-        "gamma": trial.suggest_loguniform("gamma", 0.01, 10.0),
-        "reg_alpha": trial.suggest_loguniform("reg_alpha", 1e-8, 100.0),
-        "reg_lambda": trial.suggest_loguniform("reg_lambda", 1e-8, 100.0),
-        "min_child_weight": trial.suggest_loguniform("min_child_weight", 1e-8, 100.0),
-    }
+    def objective(trial):
+        params = {
+            "n_estimators": trial.suggest_int("n_estimators", 100, 1000, step=100),
+            "max_depth": trial.suggest_int("max_depth", 3, 10),
+            "learning_rate": trial.suggest_float("learning_rate", 0.001, 0.1, log=True),
+            "subsample": trial.suggest_float("subsample", 0.6, 1.0, log=True),
+            "colsample_bytree": trial.suggest_float(
+                "colsample_bytree", 0.6, 1.0, log=True
+            ),
+            "gamma": trial.suggest_float("gamma", 0.01, 10.0, log=True),
+            "reg_alpha": trial.suggest_float("reg_alpha", 1e-8, 100.0, log=True),
+            "reg_lambda": trial.suggest_float("reg_lambda", 1e-8, 100.0, log=True),
+            "min_child_weight": trial.suggest_float(
+                "min_child_weight", 1e-8, 100.0, log=True
+            ),
+        }
 
-    model = RegressorChain(XGBRegressor(**params))
-    model.fit(X_train, y_train)
+        model = RegressorChain(XGBRegressor(**params))
+        model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
-    rmse = mean_squared_error(y_test, y_pred, squared=False)
+        y_pred = model.predict(X_test)
+        rmse = mean_squared_error(y_test, y_pred, squared=False)
 
-    return rmse
+        return rmse
 
+    study = optuna.create_study(direction="minimize", study_name="RegressorChain")
+    study.optimize(objective, n_trials=5)  # type: ignore
 
-study = optuna.create_study(direction="minimize")
-study.optimize(objective, n_trials=100)  # type: ignore
+    best_params = study.best_params
+    print(f"Best hyperparameters: {best_params}")
 
-best_params = study.best_params
-print(f"Best hyperparameters: {best_params}")
+# --- NOTICE ---
+# Best hyperparameters (0.68): {'n_estimators': 700, 'max_depth': 9, 'learning_rate': 0.041849257885856056, 'subsample': 0.8288398810318013, 'colsample_bytree': 0.8544896785347699, 'gamma': 1.5166768979741576, 'reg_alpha': 0.9719392708524538, 'reg_lambda': 32.62483746686035, 'min_child_weight': 2.6631715214066274}
+legacy_best_params = {
+    "n_estimators": 700,
+    "max_depth": 9,
+    "learning_rate": 0.041849257885856056,
+    "subsample": 0.8288398810318013,
+    "colsample_bytree": 0.8544896785347699,
+    "gamma": 1.5166768979741576,
+    "reg_alpha": 0.9719392708524538,
+    "reg_lambda": 32.62483746686035,
+    "min_child_weight": 2.6631715214066274,
+}
+final_model = RegressorChain(XGBRegressor(**legacy_best_params))
+final_model.fit(X_final, y_final)
 
-# final_model = RegressorChain(XGBRegressor(**best_params))
-# final_model.fit(X_train, y_train)
 # --- PREDICT FUTURE ---
-logger.error("Predicting future...")
+logger.info("Predicting future...")
+y_query = pd.DataFrame(
+    final_model.predict(X_query), index=X_query.index, columns=y_final.columns  # type: ignore
+)
+
+# --- SAVE PREDICTION ---
+query_families = query.index.get_level_values("family").unique()
+query_stores = query.index.get_level_values("store_nbr").unique()
+y_query_converted = pd.DataFrame()
+
+for i in range(16):
+    y_query_converted = pd.concat(
+        [
+            y_query_converted,
+            pd.concat(
+                [
+                    y_query.loc["2017-08-15"]
+                    .reset_index()[["store_nbr", "family", f"y_step_{i+1}"]]
+                    .rename(columns={f"y_step_{i+1}": "sales"}),
+                    pd.Series(
+                        [f"2017-08-{16+i}"] * len(query_families) * len(query_stores),
+                        name="date",
+                        dtype="period[D]",
+                    ),
+                ],
+                axis=1,
+            ),
+        ],
+        axis=0,
+    )
+
+
+logger.error("Saving prediction...")
+y_submission = (
+    query["id"]
+    .to_frame()
+    .join(y_query_converted.set_index(["date", "store_nbr", "family"]))
+)
+y_submission.to_csv("advanced_ml_model_submission.csv", index=False)
+logger.info("Submission saved!")
+
+
+# SOMETHING IS WRONG WITH THE MODEL
+# LEADERBOARD SCORE: 3.59 xddd
